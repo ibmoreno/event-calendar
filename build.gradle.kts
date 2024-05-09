9058f594a2e0b298dd3eefb51ec7db031e741361
Ivan Batista Moreno <ivan.batista@arcotech.io>
2024-05-09 10:09:34 -0400

    adicionado spring boot actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-configuration-processor")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.awspring.cloud:spring-cloud-aws-dependencies:$springCloudAwsVersion")
 	implementation("io.awspring.cloud:spring-cloud-aws-starter-sqs:$springCloudAwsVersion")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
